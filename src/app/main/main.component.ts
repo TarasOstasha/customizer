3655103c692d7fdceb6d256f916534943ead9b7d
TarasOstasha <tonyjoss1990@gmail.com>
2021-01-30 14:36:31 -0500

    add full color logics

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Size } from '../models/size';
 
 
 @Component({
@@ -7,14 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.less']
 })
 export class MainComponent implements OnInit {
+  // flags to change select state
   colorFlag: boolean;
+  fullColor: boolean;
   materialFlag: boolean;
+  oneSidedMaterialFlag: boolean;
+  twoSidedMaterialFlag: boolean;
 
+  enteredWidthFt: number;
+  enteredWidthIn: number;
+  enteredHeightFt: number;
+  enteredHeightIn: number;
+  enteredProdMaterial: string;
+  enteredProdType: string;
+
+  sizeData: Size[] = [];
+
+  // for blank color selected
   materials = [
     { value: '10 oz Vinyl' },
     { value: '13 oz Vinyl (Most Popular)' },
     { value: '13 oz Smooth Vinyl' },
     { value: '18 oz Opaque Vinyl' }
+  ]
+  // for fill color selected
+  oneSidedMaterial = [
+    { value: '9 oz Mesh Vinyl' },
+    { value: '10 oz Vinyl' },
+    { value: '13 oz Vinyl (Most Popular)' },
+    { value: '13 oz Smooth Vinyl' },
+    { value: '18 oz Vinyl' },
+    { value: 'Poly-Poplin Fabric' },
+    { value: 'Super Poly Knit Fabric' }
+  ]
+  twoSidedMaterial = [
+    { value: '13 oz Smooth Vinyl' },
+    { value: '18 oz Opaque Vinyl (Most Popular)' },
+    { value: 'Poly-Poplin Fabric' },
+    { value: 'Super Poly Knit Fabric' }
   ]
 
   finishType = [
@@ -43,12 +76,54 @@ export class MainComponent implements OnInit {
   }
 
   chooseBlank() {
-    this.colorFlag = !this.colorFlag;
+    this.colorFlag = true;
+    this.fullColor = false;
+    this.oneSidedMaterialFlag = false;
+    this.twoSidedMaterialFlag = false;
+  }
+
+  chooseFullColor() {
+    this.fullColor = true;
+    this.materialFlag = false;
+    this.colorFlag = false;
+  }
+
+  chooseOneSided() {
+    this.oneSidedMaterialFlag = true;
+    this.twoSidedMaterialFlag = false;
+  }
+
+  chooseTwoSided() {
+    this.twoSidedMaterialFlag = true;
+    this.oneSidedMaterialFlag = false;
   }
 
   selectedMaterial(val) {
     console.log('material- ', val)
     this.materialFlag = true;
+    this.enteredProdMaterial = val;
+  }
+
+  selectedType(val) {
+    this.enteredProdType = val;
+  }
+
+
+
+  sendData(form: NgForm) {
+    this.sizeData.push({ 
+      widthFt: this.enteredWidthFt, 
+      widthIn: this.enteredWidthIn, 
+      heightFt: this.enteredHeightFt, 
+      heightIn: this.enteredHeightIn,
+      prodMaterial: this.enteredProdMaterial,
+      prodType: this.enteredProdType 
+    });
+    console.log(this.sizeData);
+    form.resetForm();
+  }
+  resetForm() {
+
   }
 
 }
---
+++ src/app/models/size.ts
@@ -0,0 +1,8 @@
+export interface Size {
+    widthFt: number;
+    widthIn: number;
+    heightFt: number;
+    heightIn: number;
+    prodMaterial: string;
+    prodType: string;
+}
