29c51178505ff22c305fef3ae1b5dc90fb833826
TarasOstasha <tonyjoss1990@gmail.com>
2021-02-16 21:26:41 -0500

    add

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const http = require('http');
 const app = require('./backend/app');
 const debug = require('debug')('node-angular');
 
-const path = require('path');
+//const path = require('path');
 //console.log(path.join( __dirname, ''))
-app.use(express.static(path.join(__dirname, 'dist/customizer')));
+//app.use(express.static(path.join(__dirname, 'dist/customizer')));
 
 const normalizePort = val => {
     var port = parseInt(val, 10);
