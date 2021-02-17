5318d8abf01389f75e1d8b42f3841d166d7f9350
TarasOstasha <tonyjoss1990@gmail.com>
2021-02-16 23:10:11 -0500

    deploy

--- backend/app.js
+++ backend/app.js
@@ -20,22 +20,30 @@ const cors = require('cors');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'assets/imgs')));
+app.use('/', express.static(path.join(__dirname, 'dist')));
+
+// routes
 app.use('/api', optionRoutes);
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 //console.log(app.use(express.static('../dist')))
 //app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', express.static('../dist'));
+//app.use('/', express.static('../dist'));
 //app.use(express.static(path.join(__dirname, 'node_modules')));
 //app.use(express.static(path.join(__dirname, 'dist')));
 // For static files
-app.use(express.static('../dist'));
-app.set('view engine', 'html');
-app.engine('html', require('ejs').renderFile);
-// For virtual routes use 
-app.use('*', (req, res) => 
-{
-  res.render(__dirname +'/dist/index.html');
- });
+// app.use(express.static('../dist'));
+// app.set('view engine', 'html');
+// app.engine('html', require('ejs').renderFile);
+// // For virtual routes use 
+// app.use('*', (req, res) => 
+// {
+//   res.render(__dirname +'/dist/index.html');
+//  });
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
